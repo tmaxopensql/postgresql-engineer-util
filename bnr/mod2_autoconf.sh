7ae632cbd5eb300ef2799382f30aa38f8d75d445
HWLEE-PS <101083423+HWLEE-PS@users.noreply.github.com>
2022-09-14 11:17:02 +0900

    4번 파라미터 추가
    4번 파라미터는 백업 파일의 autoconf을 직접 수정하고 recovery.signal을 생성할 수 있도록 바꿈

--- bnr/mod2_autoconf.sh
+++ bnr/mod2_autoconf.sh
@@ -8,10 +8,22 @@
 
 # 기존 값 주석 처리 
 if [[ "${#3}" -gt 0 && $3 -eq 1 ]]; then
-  sed -i "/recovery/d" $PGDATA/postgresql.auto.conf
+  if [[ "${#4}" -gt 0 ]]; then
+    sed -i "/recovery/d" ${4}/postgresql.auto.conf
+    sed -i "/restore/d" ${4}/postgresql.auto.conf
+  else
+    sed -i "/recovery/d" $PGDATA/postgresql.auto.conf
+    sed -i "/restore/d" $PGDATA/postgresql.auto.conf
+  fi
 fi
 
 # Value Insert
-echo "SET ${1} = ${2} postgresql.auto.conf"
-echo "${1} = '${2}'" >> $PGDATA/postgresql.auto.conf
-touch $PGDATA/recovery.signal
+echo "SET ${1} = ${2}"
+
+if [[ "${#4}" -gt 0 ]]; then
+  echo "${1} = '${2}'" >> ${4}/postgresql.auto.conf
+  touch ${4}/recovery.signal
+else
+  echo "${1} = '${2}'" >> $PGDATA/postgresql.auto.conf
+  touch $PGDATA/recovery.signal
+fi
