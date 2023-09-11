3dccfd2f809b1efdefe13bd3c5f0c84736169122
HWLEE-PS <101083423+HWLEE-PS@users.noreply.github.com>
2023-09-11 17:45:18 +0900

    Host Check Retry 추가, Archive Cleanup 추가, 백업 주기 설정 확장, 인포메이션 조회에 사용할 데이터베이스 추가
    1. Host Check Retry 추가
     - CON_RETRY_ROLE 파라미터 추가
     - 설정한 HOST ROLE(Primary, Standby) 실패 시 다른 역할의 노드에서 백업 수행
    
    2. Archive Cleanup 추가
     - BAK_REMOVE_ARCHIVE 파라미터 추가
     - BAK_ARCHIVE_DIR 파라미터 추가
     - 백업 수행 이후 아카이브 클린업 수행
    
    3. 백업 주기 설정 확장
     - BAK_PERIOD 설정 범위 4 추가
     - BAK_DAY_OF_MONTH 추가
     - BAK_DAY_OF_WEEK 추가
     - BAK_TIME_HOUR 추가
     - BAK_TIME_MINUTE 추가
    
    4. CON_DATABASE 추가
     - 유저명과 같은 데이터베이스를 참조하여, 카탈로그 조회가 실패하는 현상이 발생하여 추가하였음.

--- backup/opensql_backup.sh
+++ backup/opensql_backup.sh
@@ -2,6 +2,7 @@
 PATH=/usr/pgsql-14/bin/:$PATH
 DATETIME=`date +%Y%m%d_%H%M%S`
 CON_CHK_FLAG="t"
+RETRY_ROLE=0
 
 # LOGGING
 function logging() {
@@ -18,9 +19,9 @@ logging "Exec get_DB_info()..."
                 logging "\n| Database List "
                 logging "-----------------"
     shopt -s lastpipe
-    psql -q --host=${CON_HOST} --port=${CON_PORT} --username=${CON_USER} -x -A -l | grep Name | while read line
+    psql -q --host=${CON_HOST} --port=${CON_PORT} --username=${CON_USER} -d ${CON_DATABASE} -x -A -l | grep Name | while read line
     do
-        psql -q --host=${CON_HOST} --port=${CON_PORT} --username=${CON_USER} -xAtc "SELECT pg_database_size('${line:5}') AS size" | read size
+        psql -q --host=${CON_HOST} --port=${CON_PORT} --username=${CON_USER} -d ${CON_DATABASE} -xAtc "SELECT pg_database_size('${line:5}') AS size" | read size
                 logging "| ${line:5} "$((${size:5}/1024/1024))"MB"
         sizeSum=$((${sizeSum} + ${size:5}))
     done
@@ -33,7 +34,7 @@ function print_Progress() {
 	sleep $1
 	while [[ -f ~/.progress.lock ]]; 
 	do
-		PROGRESS=`psql -h ${CON_HOST} -p ${CON_PORT} -U ${CON_USER} -c "SELECT bak_info.total||'MB' as TOTAL_BACKUP_SIZE, bak_info.now||'MB' AS RECEIVED_SIZE, bak_info.tbs_total AS TOTAL_TABLESPACE_COUNT, bak_info.tbs_now AS RECEIVED_TABLESPACE_COUNT, (bak_info.now*100/bak_info.total)||'%' AS PROGRESS FROM (SELECT CASE backup_total WHEN NULL THEN 0 ELSE backup_total/1024/1024 END AS total, CASE backup_streamed WHEN NULL THEN 0 ELSE backup_streamed/1024/1024 END AS now, CASE tablespaces_total WHEN NULL THEN 0 ELSE tablespaces_total END AS tbs_total, CASE tablespaces_streamed WHEN NULL THEN 0 ELSE tablespaces_streamed END AS tbs_now FROM pg_stat_progress_basebackup)as bak_info"`
+		PROGRESS=`psql -h ${CON_HOST} -p ${CON_PORT} -U ${CON_USER} -d ${CON_DATABASE} -c "SELECT bak_info.total||'MB' as TOTAL_BACKUP_SIZE, bak_info.now||'MB' AS RECEIVED_SIZE, bak_info.tbs_total AS TOTAL_TABLESPACE_COUNT, bak_info.tbs_now AS RECEIVED_TABLESPACE_COUNT, (bak_info.now*100/bak_info.total)||'%' AS PROGRESS FROM (SELECT CASE backup_total WHEN NULL THEN 0 ELSE backup_total/1024/1024 END AS total, CASE backup_streamed WHEN NULL THEN 0 ELSE backup_streamed/1024/1024 END AS now, CASE tablespaces_total WHEN NULL THEN 0 ELSE tablespaces_total END AS tbs_total, CASE tablespaces_streamed WHEN NULL THEN 0 ELSE tablespaces_streamed END AS tbs_now FROM pg_stat_progress_basebackup)as bak_info"`
 		logging "\n$PROGRESS"
 	sleep $1
 	done
@@ -50,10 +51,10 @@ function calculate_Time() {
 # 2022-12-08 
 function tablespace_remapping() {
 logging "Exec tablespace_remapping()..."
-        LOCS=`psql -q --host=${CON_HOST} --port=${CON_PORT} --username=${CON_USER} -x -A -t -c "\db+"`
+        LOCS=`psql -q --host=${CON_HOST} --port=${CON_PORT} --username=${CON_USER} -d ${CON_DATABASE}  -x -A -t -c "\db+"`
         logging "Tablespace info :"
 	echo -e "Tablespace info :\n" >> ${BAK_LOG_DIR}/tbs_remap_info
-	`psql -q --host=${CON_HOST} --port=${CON_PORT} --username=${CON_USER} -x -A -t -c "SELECT oid as SymboliclinkName, spcname as TablespaceName FROM pg_catalog.pg_tablespace" >> ${BAK_LOG_DIR}/tbs_remap_info` 
+	`psql -q --host=${CON_HOST} --port=${CON_PORT} --username=${CON_USER} -d ${CON_DATABASE} -x -A -t -c "SELECT oid as SymboliclinkName, spcname as TablespaceName FROM pg_catalog.pg_tablespace" >> ${BAK_LOG_DIR}/tbs_remap_info` 
 	logging "\n$LOCS"
 	echo ${BAK_DIR}/tbs_remap/tbs_remap_info
 	echo -e "\n$LOCS" >> ${BAK_LOG_DIR}/tbs_remap_info
@@ -76,6 +77,7 @@ case $1 in
         1) echo "0 0 * * * " ;;
         2) echo "0 0 * * 7 " ;;
         3) echo "0 0 1 * * " ;;
+	4) echo "${BAK_TIME_MINUTE} ${BAK_TIME_HOUR} ${BAK_DAY_OF_MONTH} * ${BAK_DAY_OF_MONTH}" ;;
 esac
 }
 
@@ -89,6 +91,8 @@ logging "Exec calPeriod()..."
 	sed -i "/opensql_backup/d" ${BAK_DIR}/crontmpf
         echo "$CRON_CMD" >> ${BAK_DIR}/crontmpf
         crontab -r
+        crontab -r
+        crontab -r
         crontab -i ${BAK_DIR}/crontmpf
         logging "$CRON_CMD"
         rm -rf ${BAK_DIR}
@@ -99,18 +103,20 @@ function checkHost() {
 if [[ ${CON_TYPE} =~ [pP] ]] || [[ ${CON_TYPE} =~ [sS] ]]; then
         for PHOST in ${CON_HOST[@]};
         do
-            RECV_MODE=`psql -q -h ${PHOST} -p ${CON_PORT} -U ${CON_USER} -A -t -c "SELECT pg_is_in_recovery()"`
+            RECV_MODE=`psql -q -h ${PHOST} -p ${CON_PORT} -U ${CON_USER} -d ${CON_DATABASE} -A -t -c "SELECT pg_is_in_recovery()"`
 		CON_CHK_FLAG="t"
 		if [[ ${CON_TYPE} =~ [pP] ]] && [[ $RECV_MODE == f ]]; then
 		    CON_HOST=${PHOST}
+		    break;
 		elif [[ ${CON_TYPE} =~ [sS] ]] && [[ $RECV_MODE == t ]]; then	
 		    CON_HOST=${PHOST}
+		    break;
 		else
 		    CON_CHK_FLAG="f"
 		fi
         done
 elif [[ ${CON_TYPE} =~ [lL] ]]; then
-	RECV_MODE=`psql -q -h ${CON_HOST} -p ${CON_PORT} -U ${CON_USER} -A -t -c "SELECT pg_is_in_recovery()"`
+	RECV_MODE=`psql -q -h ${CON_HOST} -p ${CON_PORT} -U ${CON_USER} -d ${CON_DATABASE} -A -t -c "SELECT pg_is_in_recovery()"`
 	CON_CHK_FLAG="t"
         if  [[ $RECV_MODE != f ]]; then
                     CON_CHK_FLAG="f"
@@ -119,13 +125,36 @@ elif [[ ${CON_TYPE} =~ [lL] ]]; then
 fi
 }
 
+function checkHostRetry(){
+        CON_CHK_COUNT=0
+        while [[ ${CON_CHK_COUNT} -le ${CON_RETRY_COUNT} ]] && [[ ${CON_CHK_FLAG} == "f" ]];
+        do
+		logging "RETRY CHECK CONNECTION $((CON_CHK_COUNT+1)) TIMES"
+                if [[ $CON_RETRY_TIME != "" ]] && [[ ${CON_RETRY_TIME} -ge 0 ]]; then
+                        sleep ${CON_RETRY_TIME}
+                else
+                        sleep 10
+                fi
+                checkHost
+                CON_CHK_COUNT=$((CON_CHK_COUNT+1))
+        done
+}
+
+
 function checkBackup() {
-	BACKUP_PROGRESS=`psql -q -h ${CON_HOST} -p ${CON_PORT} -A -t -c "SELECT * FROM pg_stat_progress_basebackup"`
+	BACKUP_PROGRESS=`psql -q -h ${CON_HOST} -p ${CON_PORT} -d ${CON_DATABASE} -A -t -c "SELECT * FROM pg_stat_progress_basebackup"`
 	if [[ ${BACKUP_PROGRESS} != "" ]]; then
 		logging "Another backup is in progress..."
 		exit 4
         fi
 }
+
+function archive_cleanup() {
+	OLDEST_WAL=`pg_controldata ${BAK_DIR} | grep "REDO WAL"`
+	OLDEST_WAL=${OLDEST_WAL#*:}
+	logging `pg_archivecleanup ${BAK_ARCHIVE_DIR} ${OLDEST_WAL} -d`
+}
+
 # ECHO USAGE
 if [ $# -lt 1 ] || [ $# -gt 2 ]; then
         echo "Usage: $0 <CONFIG FILE> [OPTION]"
@@ -219,33 +248,41 @@ if [[ ! ${CON_TYPE} =~ [pP] ]] && [[ ! ${CON_TYPE} =~ [sS] ]] && [[ ! ${CON_TYPE
 fi
 
 checkHost
-
 if [[ ${CON_CHK_FLAG} == "f" ]] && [[ ${CON_RETRY_COUNT} != "" ]] && [[ ${CON_RETRY_COUNT} -gt 1 ]]; then
-        CON_CHK_COUNT=0
-        while [[ ${CON_CHK_COUNT} -le ${CON_RETRY_COUNT} ]] && [[ ${CON_CHK_FLAG} == "f" ]];
-        do
-                if [[ $CON_RETRY_TIME != "" ]] && [[ ${CON_RETRY_TIME} -ge 0 ]]; then
-                        sleep ${CON_RETRY_TIME}
-                else
-                        sleep 10
-                fi
-                checkHost
-                CON_CHK_COUNT=$((CON_CHK_COUNT+1))
-        done
+	checkHostRetry
+fi
+
+
+if [[ ${CON_CHK_FLAG} == "f" ]] && [[ ${CON_RETRY_ROLE} =~ [yY] ]]; then
+	if [[ ${CON_TYPE} =~ [pP] ]]; then 
+		CON_TYPE=S
+	elif [[ ${CON_TYPE} =~ [sS] ]]; then
+		CON_TYPE=P
+	fi	
+        logging "Retry WITH CON_TYPE : ${CON_TYPE}"
+	RETRY_ROLE=1
+	checkHostRetry
 fi
 
 if [[ ${CON_CHK_FLAG} == "f" ]]; then
         logging "[ERR:04] : Please, check connection to host"
         exit 4;
 fi
-BAK_OPTS="-h ${CON_HOST} -p ${CON_PORT} -U ${CON_USER} $BAK_OPTS"
+BAK_OPTS="-h ${CON_HOST} -p ${CON_PORT} -U ${CON_USER} $BAK_OPTS "
 
 
 # SET PERIOD
 logging "SET PERIOD..."
-if [[ ! $BAK_PERIOD =~ ^[0-3] ]] || [[ ${#BAK_PERIOD} -gt 1 ]]; then
+if [[ ! ${BAK_PERIOD} =~ ^[0-4] ]] || [[ ${#BAK_PERIOD} -gt 1 ]]; then
         logging "[ERR:02] : Please, Input valid period in configuration file"
         exit 2
+	if [[ ${BAK_PERIOD} -eq 4 ]] && [[ ! ${BAK_PERIOD} =~ [0-9*,] ]] ; then
+		logging "[ERR:02] : Please, Input valid period in configuration file"
+	        exit 2
+	fi
+fi
+if [[ ${BAK_PERIOD} -ne 0 ]]; then
+	rm -r ${BAK_DIR}
 fi
 
 
@@ -302,11 +339,27 @@ case $BAK_PERIOD in
 		fi
 		pg_basebackup ${BAK_OPTS} >> ${BAK_LOG_DIR}/backup-${DATETIME}.log 2>&1
 		if [[ $? -eq 0 ]]; then
-                	logging "BACKUP COMPLETE.." 
+                	logging "BASEBACKUP COMPLETE" 
+                	logging "VERIFYING BACKUP.."
+			pg_verifybackup ${BAK_DIR} >> ${BAK_LOG_DIR}/backup-${DATETIME}.log 2>&1
+			VERIFY_SUCCESS=1
+			if [[ $? -eq 0 ]]; then
+                		logging "BACKUP SUCCESSFULLY VERIFIED"
+			else
+                		logging "FAILED TO VERIFY BACKUP!! PLEASE SEE LOGS!!"
+			fi
 			if [[ -f ${BAK_LOG_DIR}/tbs_remap_info ]]; then	
 				mv ${BAK_LOG_DIR}/tbs_remap_info ${BAK_DIR}/tbs_remap_info
 				if [[ -d ${BAK_DIR}/pg_tblspc ]] && [[ ! -e ${BAK_DIR}/pg_tblspc ]]; then
 					rm ${BAK_DIR}/pg_tblspc/*
+				fi
+			fi
+			if [[ ${BAK_REMOVE_ARCHIVE} =~ [yY] ]]; then
+				if [[ $VERIFY_SUCCESS -eq ]]; then
+	                		logging "ARCHIVING FILES WILL BE CLEANED"
+					archive_cleanup
+				else
+					logging "VERIFYING BACKUP FAILED, ARCHIVING FILES WILL NOT BE CLEANED"
 				fi
 			fi
 		else
