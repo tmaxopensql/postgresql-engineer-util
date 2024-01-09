29c5b026d6bb3f8d6cc245f7566a4ba849cccbd5
MINSIKKANG <108848567+MINSIKKANG@users.noreply.github.com>
2024-01-09 16:06:33 +0900

    Update 14_vacuumcheck.sql

--- TOSPbinary/sql/14/14_vacuumcheck.sql
+++ TOSPbinary/sql/14/14_vacuumcheck.sql
@@ -1,24 +1,25 @@
 
 /* vacuum used - vacuum verbose 'tablename'*/
-select 
-	relname, 
-	last_vacuum, 
-	last_autovacuum, 
-	vacuum_count, 
-	autovacuum_count
-from 
-	pg_stat_user_tables
+select
+        relname,
+        TO_CHAR(last_vacuum, 'YYYY-MM-DD HH24:MI:SS') AS last_vacuum,
+        TO_CHAR(last_autovacuum, 'YYYY-MM-DD HH24:MI:SS') AS last_autovacuum,
+        vacuum_count,
+        autovacuum_count
+from
+        pg_stat_user_tables
 order by last_vacuum, last_autovacuum;
 
 
 /* vacuum analyze command used - vacuum analyze 'tablename'*/
-select 
-	relname, 
-	last_analyze, 
-	last_autoanalyze, 
-	analyze_count, 
-	autoanalyze_count 
-from 
-	pg_stat_user_tables 
+select
+        relname,
+        TO_CHAR(last_analyze, 'YYYY-MM-DD HH24:MI:SS') AS last_analyze,
+        TO_CHAR(last_autoanalyze, 'YYYY-MM-DD HH24:MI:SS') AS last_autoanalyze,
+        analyze_count,
+        autoanalyze_count
+from
+        pg_stat_user_tables
 order by last_analyze, last_autoanalyze;
+
 
