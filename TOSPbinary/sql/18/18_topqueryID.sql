df7fbd892086ca8e0020a3e77638d062c2c9c174
Hans Choi <100177857+jaehanchoi@users.noreply.github.com>
2024-01-18 16:24:46 +0900

    Update 18_topqueryID.sql
    디스크 사용량이 많은 쿼리 id 조회를 위한 local_blks_read 컬럼으로 정렬하는 쿼리 추가

--- TOSPbinary/sql/18/18_topqueryID.sql
+++ TOSPbinary/sql/18/18_topqueryID.sql
@@ -19,6 +19,21 @@ where
 order by a.max_exec_time desc
 limit 10;
 
+select 
+	b.usename, 
+	c.datname, 
+	a.queryid,
+	substr(a.query,1,50) as query_simple
+from 
+	public.pg_stat_statements a
+join pg_catalog.pg_user b on a.userid = b.usesysid
+join pg_catalog.pg_stat_database c on a.dbid = c.datid
+where 
+	c.datname = :v1
+order by a.local_blks_read desc
+limit 10;
+
+
 select pg_sleep(5);
 
 end;
