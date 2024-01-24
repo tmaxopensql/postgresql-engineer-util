4f1cba458ea9adb4c4b94abb8bc97d2ca9c4380c
Hans Choi <100177857+jaehanchoi@users.noreply.github.com>
2024-01-24 14:43:18 +0900

    Update 02_tablespace.sql
    가독성을 위한 쿼리 수정

--- TOSPbinary/sql/02/02_tablespace.sql
+++ TOSPbinary/sql/02/02_tablespace.sql
@@ -1,13 +1,13 @@
-select 
-	oid AS OID,
-	spcname AS "Tablespace name",
-	pg_catalog.pg_get_userbyid(spcowner) AS "Owner",
-	case
-	when spcname='pg_default' then :v1
-	when spcname='pg_global' then :v2
-	else pg_catalog.pg_tablespace_location(oid) 
-	end AS "Location",
-	pg_size_pretty(pg_tablespace_size(spcname)) as tablespace_size
+select
+        oid AS "TABLESPACE OID",
+        spcname AS "Tablespace name",
+        pg_catalog.pg_get_userbyid(spcowner) AS "TABLESPACE OWNER",
+	pg_size_pretty(pg_tablespace_size(spcname)) as "TABLESPACE SIZE",
+        case
+       		when spcname='pg_default' then :v1
+        	when spcname='pg_global' then :v2
+        	else pg_catalog.pg_tablespace_location(oid)
+        end AS "LOCATION"
 FROM
- 	pg_catalog.pg_tablespace
+        pg_catalog.pg_tablespace
 ORDER BY 1;
