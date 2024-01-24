53dc8654c1e444b63df048089e66843c71477f25
Hans Choi <100177857+jaehanchoi@users.noreply.github.com>
2024-01-24 15:38:36 +0900

    Update 06_processmemory.sql
    명확한 정보 제공을 위한 쿼리 변경

--- TOSPbinary/sql/06/06_processmemory.sql
+++ TOSPbinary/sql/06/06_processmemory.sql
@@ -16,3 +16,14 @@ show work_mem;
 show maintenance_work_mem;
 
 show temp_buffers;
+
+--hans 2024-01-24
+SELECT
+	case when cast(a.setting as integer) = -1 then pg_size_pretty(cast(b.setting as bigint)*1024)
+	     when cast(a.setting as integer) > 0 then pg_size_pretty(cast(a.setting as bigint)*1024)
+	end as autovacuum_work_mem,
+	c.setting as autovacuum_max_workers
+FROM 
+	pg_settings a, pg_settings b , pg_settings c
+WHERE
+	a.name = 'autovacuum_work_mem' and b.name = 'maintenance_work_mem' and c.name='autovacuum_max_workers';
