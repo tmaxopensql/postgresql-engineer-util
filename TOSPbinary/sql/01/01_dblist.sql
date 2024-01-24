3f7b159b565732eec082b259593341913af2da8e
Hans Choi <100177857+jaehanchoi@users.noreply.github.com>
2024-01-24 14:41:24 +0900

    Update 01_dblist.sql
    가독성 위한 쿼리 수정

--- TOSPbinary/sql/01/01_dblist.sql
+++ TOSPbinary/sql/01/01_dblist.sql
@@ -1,17 +1,21 @@
 select 
-	s.usename as owner, 
-	d.datname as database_name, 
-	pg_encoding_to_char(d.encoding) as encoding, 
-	d.datcollate as colate, 
-	d.datctype as ctype, 
-	d.datacl as acl_auth
+        s.usename as "ROLE", 
+        d.datname as "DATABASE NAME", 
+        pg_encoding_to_char(d.encoding) as "ENCODING", 
+        d.datcollate as "COLATE", 
+        d.datctype as "CTYPE", 
+        d.datacl as "ACL_AUTH"
 from 
-	pg_database d, pg_shadow s
+        pg_database d, pg_shadow s
 where 
-	s.usesysid = d.datdba;
+        s.usesysid = d.datdba;
 
-
-select 
-	usename as owner_list 
+select
+        usename as "ROLE LIST",
+        usesysid as "ROLE OID",
+        usecreatedb as "CREATEDB",
+        usesuper as "SUPERUSER",
+        userepl as "REPLICAION",
+        usebypassrls as "BYPASSRLS"
 from 
-	pg_shadow;
+        pg_shadow;
