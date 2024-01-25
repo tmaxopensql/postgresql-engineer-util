125529f2f461e88829d8199f0ef48d2bdffc8ff7
Hans Choi <100177857+jaehanchoi@users.noreply.github.com>
2024-01-25 17:20:07 +0900

    Update 14_vacuumcheck.sql
    조회 컬럼과 정렬 순서 재정립 및 limit 추가

--- TOSPbinary/sql/14/14_vacuumcheck.sql
+++ TOSPbinary/sql/14/14_vacuumcheck.sql
@@ -1,25 +1,23 @@
+SELECT
+        relname as "TABLE",
+        schemaname "SCHEMA",
+        TO_CHAR(last_vacuum, 'YYYY-MM-DD HH24:MI:SS') as "Last Vacuum Time",
+        TO_CHAR(last_autovacuum, 'YYYY-MM-DD HH24:MI:SS') as "Last Autovacuum Time",
+        vacuum_count as "Vacuum Count",
+        autovacuum_count as "Autovacuum Count"
+FROM
+        pg_stat_user_tables
+ORDER BY last_vacuum, last_autovacuum
+LIMIT 20;
 
-/* vacuum used - vacuum verbose 'tablename'*/
-select
-        relname,
-        TO_CHAR(last_vacuum, 'YYYY-MM-DD HH24:MI:SS') AS last_vacuum,
-        TO_CHAR(last_autovacuum, 'YYYY-MM-DD HH24:MI:SS') AS last_autovacuum,
-        vacuum_count,
-        autovacuum_count
+SELECT
+        relname as "TABLE",
+        schemaname "SCHEMA",
+        TO_CHAR(last_analyze, 'YYYY-MM-DD HH24:MI:SS') as "Last Analyze Time",
+        TO_CHAR(last_autoanalyze, 'YYYY-MM-DD HH24:MI:SS') as "Last Autoanlayze Time",
+        analyze_count as "Analyze Count",
+        autoanalyze_count as "Autoanalyze Count"
 from
         pg_stat_user_tables
-order by last_vacuum, last_autovacuum;
-
-
-/* vacuum analyze command used - vacuum analyze 'tablename'*/
-select
-        relname,
-        TO_CHAR(last_analyze, 'YYYY-MM-DD HH24:MI:SS') AS last_analyze,
-        TO_CHAR(last_autoanalyze, 'YYYY-MM-DD HH24:MI:SS') AS last_autoanalyze,
-        analyze_count,
-        autoanalyze_count
-from
-        pg_stat_user_tables
-order by last_analyze, last_autoanalyze;
-
-
+order by last_analyze, last_autoanalyze
+limit 20;
