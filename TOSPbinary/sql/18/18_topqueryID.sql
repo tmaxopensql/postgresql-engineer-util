4dea6f16293d93e713cc6af9833364164a5ca33b
Hans Choi <100177857+jaehanchoi@users.noreply.github.com>
2024-01-25 18:28:59 +0900

    Update 18_topqueryID.sql
    수정중

--- TOSPbinary/sql/18/18_topqueryID.sql
+++ TOSPbinary/sql/18/18_topqueryID.sql
@@ -1,39 +1,40 @@
 /* total top 10 query*/
+-- 수정중!! 2024-01-25
+BEGIN;
 
-begin;
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
 
-create extension if not exists pg_stat_statements;
-
-
-select 
-	b.usename, 
-	c.datname, 
-	a.queryid,
-	substr(a.query,1,50) as query_simple
-from 
-	public.pg_stat_statements a
+SELECT
+        b.usename as "ROLE",
+        c.datname as "DATABASE NAME",
+        a.queryid as "QUERY ID",
+        a.max_exec_time as "MAX EXEC TIME (MS)",
+        substr(a.query,1,50) as "SIMPLE QUERY"
+FROM
+        public.pg_stat_statements a
 join pg_catalog.pg_user b on a.userid = b.usesysid
 join pg_catalog.pg_stat_database c on a.dbid = c.datid
-where 
-	c.datname = :v1
+WHERE
+        c.datname = :v1
 order by a.max_exec_time desc
 limit 10;
 
-select 
-	b.usename, 
-	c.datname, 
-	a.queryid,
-	substr(a.query,1,50) as query_simple
-from 
-	public.pg_stat_statements a
+SELECT
+        b.usename as "ROLE",
+        c.datname as "DATABASE NAME",
+        a.queryid as "QUERY ID",
+        a.local_blks_read as "LOCAL BLOCK READ",
+        substr(a.query,1,50) as "SIMPLE QUERY"
+FROM
+        public.pg_stat_statements a
 join pg_catalog.pg_user b on a.userid = b.usesysid
 join pg_catalog.pg_stat_database c on a.dbid = c.datid
-where 
-	c.datname = :v1
+WHERE
+        c.datname = :v1
 order by a.local_blks_read desc
 limit 10;
 
 
 select pg_sleep(5);
 
-end;
+ROLLBACK;
